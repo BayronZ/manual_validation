a3e1ea67686ced958f054cd9e6b4c1e41250af2d
BayronZ <bayron.zavalag@gmail.com>
2022-06-16 23:04:48 -0400

    added migration belongs to

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,4 @@
 class Story < ApplicationRecord
-    # belongs_to :user
+    belongs_to :user
     # validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_060812) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_17_020926) do
   create_table "stories", force: :cascade do |t|
     t.string "title"
     t.string "picture"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_stories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_060812) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "stories", "users"
 end
